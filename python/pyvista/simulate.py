3fe2796217d7785fcf934b211d29dbd9cc9387ea
Jon Holtzman <holtz@antares.nmsu.edu>
2019-09-06 20:00:45 -0600

    add noise

--- python/pyvista/simulate.py
+++ python/pyvista/simulate.py
@@ -4,7 +4,7 @@ import numpy as np
 import pdb
 from astropy.nddata import CCDData
 
-def gauss2d(data, coords,fwhm=1,noise=False) :
+def gauss2d(data, coords,fwhm=1,back=0,noise=False,rn=0) :
     """ Add 2d gaussians to data given input coords, fwhm, noise"""
     if type(coords[0]) is not list : coords=[coords]
     pix = np.mgrid[0:data.shape[0],0:data.shape[1]]
@@ -18,5 +18,11 @@ def gauss2d(data, coords,fwhm=1,noise=False) :
         dist2 = (xpix-x)**2 + (ypix-y)**2
         gd = np.where(dist2 < 100*sig2)
         data[gd[0],gd[1]] += amp*np.exp(-dist2[gd[0],gd[1]]/(2.*sig2))
+    data = data.astype(float) + back
+
+    if noise:
+        data = np.random.poisson(data)
+        if rn>0 : data = data.astype(float) +  \
+                         rn*np.random.normal(size=data.shape)
 
     return CCDData(data,unit='photon')
