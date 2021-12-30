d4a46db97a4b3aa5193e8f92c02951a7af081a44
Jon Holtzman <u0914351@apogee.wasatch.peaks>
2021-12-29 21:47:31 -0700

    updates to apogee : include visit and do_visit processing initial routines for boss

--- python/pyvista/apogee.py
+++ python/pyvista/apogee.py
@@ -1,5 +1,6 @@
 from astropy.io import fits
 import astropy.units as u
+import esutil
 import glob
 import numpy as np
 import pdb
@@ -8,6 +9,9 @@ from pydl.pydlutils.yanny import yanny
 from tools import plots, match
 from ccdproc import CCDData
 import matplotlib.pyplot as plt
+import multiprocessing as mp
+import yaml
+from pyvista import imred, spectra,sdss
 
 def unzip(file,dark=None) :
     """ Read APOGEE .apz file, get CDS image
@@ -46,22 +50,19 @@ def unzip(file,dark=None) :
 
         ext += 1
 
-      # compute and add the cdsframe, subtract dark if we have one
-    if dark is not None :
-        # if we don't have enough reads in the dark, do nothing
-        try :
-            data3d -= dark[0:nreads]
-        except:
-            print('not halting: not enough reads in dark, skipping dark subtraction for mjdcube')
-            pass
-
     return data3d
 
 def cds(file,dark=None) :
     """ CDS extraction of a cube
     """
     header = fits.open(file)[1].header
-    cube = unzip(file,dark=dark)
+    cube = unzip(file)
+    if dark is not None :
+        try :
+            cube = cube.astype(np.float32) - dark[0:len(cube)]
+        except:
+            print('not enough reads in dark')
+            pdb.set_trace()
     out= (cube[-1,0:2048,0:2048] - cube[1,0:2048,0:2048] ).astype(np.float32)
     return CCDData(data=vert(out),header=header,unit=u.dimensionless_unscaled)
 
@@ -75,3 +76,110 @@ def vert(data) :
 
     return data
 
+def visit(planfile,tracefile=None) :
+    """ Reduce an APOGEE visit
+ 
+        Driver to do 3 chips in parallel
+        Makes median flux plots
+    """    
+
+    # reduce channels in parallel
+    chan=['a','b','c' ]
+    procs=[]
+    for channel in [1] :
+        kw={'planfile' : planfile, 'channel' : channel, 'clobber' : False}
+        procs.append(mp.Process(target=do_visit,kwargs=kw))
+    for proc in procs : proc.start()
+    for proc in procs : proc.join()
+
+    plan=yaml.load(open(planfile,'r'), Loader=yaml.FullLoader)
+
+    fig,ax=plots.multi(1,1)
+    allmags=[]
+    allinst=[]
+    for ichan,channel in enumerate([1]) :
+      mags=[]
+      inst=[]
+      for obj in plan['APEXP'] :
+        if obj['flavor']  != 'object' : continue
+        name='ap1D-{:s}-{:08d}.fits'.format(chan[channel],obj['name'])
+        out=CCDData.read(name)
+        print(name,out.header['NREAD'])
+        mapname=plan['plugmap']
+        if np.char.find(mapname,'conf') >=0 :
+            plug=sdss.config(out.header['CONFIGID'],specid=2)
+            hmag=plug['h_mag']
+        else :
+            plug=sdss.config(os.environ['MAPPER_DATA_N']+'/'+mapname.split('-')[1]+'/plPlugMapM-'+mapname+'.par',specid=2,struct='PLUGMAPOBJ')
+            plate=int(mapname.split('-')[0])
+            holes=yanny('{:s}/plates/{:04d}XX/{:06d}/plateHolesSorted-{:06d}.par'.format(
+                  os.environ['PLATELIST_DIR'],plate//100,plate,plate))
+            h=esutil.htm.HTM()
+            m1,m2,rad=h.match(plug['ra'],plug['dec'],holes['STRUCT1']['target_ra'],holes['STRUCT1']['target_dec'],0.1/3600.,maxmatch=500)
+            hmag=plug['mag'][:,1]
+            hmag[m1]=holes['STRUCT1']['tmass_h'][m2]
+
+        i1,i2=match.match(300-np.arange(300),plug['fiberId'])
+        mag='H'
+        rad=np.sqrt(plug['xFocal'][i2]**2+plug['yFocal'][i2]**2)
+        plots.plotp(ax,hmag[i2],+2.5*np.log10(np.median(out.data/(out.header['NREAD']-2),axis=1))[i1],color=None,
+                    zr=[0,300],xr=[8,15],size=20,label=name,xt=mag,yt='-2.5*log(cnts/read)')
+        mags.append(hmag[i2])
+        inst.append(-2.5*np.log10(np.median(out.data/(out.header['NREAD']-2),axis=1))[i1])
+      ax.grid()
+      ax.legend()
+      allmags.append(mags)
+      allinst.append(inst)
+    fig.suptitle(planfile)
+    fig.tight_layout()
+    fig.savefig(planfile.replace('.yaml','.png'))
+    return allmags,allinst
+    
+def do_visit(planfile=None,channel=0,clobber=False,nfibers=300,threads=12) :
+    """ Read raw image (eventually, 2D calibration) and extract,
+        using specified flat/trace
+    """
+    chan=['a','b','c' ]
+    plan=yaml.load(open(planfile,'r'), Loader=yaml.FullLoader)
+    # are all files already created?
+    done =True
+    for obj in plan['APEXP'] :
+        if obj['flavor']  != 'object' : continue
+        name='ap1D-{:s}-{:08d}.fits'.format(chan[channel],obj['name'])
+        if not os.path.exists(name) or clobber :  done = False
+    if done :  return
+
+    # set up Reducer
+    red=imred.Reducer('APOGEE',dir=os.environ['APOGEE_DATA_N']+'/'+str(plan['mjd']))
+    # get Dark
+    name='apDark-{:s}-{:08d}.fits'.format(chan[channel],plan['darkid'])
+    dark=fits.open('{:s}/{:s}/cal/{:s}/darkcorr/{:s}'.format(os.environ['APOGEE_REDUX'],plan['apogee_drp_ver'],plan['instrument'],name))[1].data
+
+    # get Trace/PSF if needed
+    name='apTrace-{:s}-{:08d}.fits'.format(chan[channel],plan['psfid'])
+    if os.path.exists(name) and not clobber : 
+        trace=spectra.Trace('./'+name)
+    else :
+        flat=red.reduce(plan['psfid'],channel=channel,dark=dark)
+        trace=spectra.Trace(transpose=red.transpose,rad=3,lags=np.arange(-3,4))
+        ff=np.sum(flat.data[:,1000:1100],axis=1)
+        if channel==0 : thresh=40000
+        else : thresh=40000
+        peaks,fiber=spectra.findpeak(ff,thresh=thresh)
+        print('found {:d} peaks'.format(len(peaks)))
+        trace.trace(flat,peaks[0:nfibers],index=fiber[0:nfibers],skip=4)
+        pdb.set_trace()
+        trace.write(name)
+
+    # now reduce and extract
+    for obj in plan['APEXP'] :
+        if obj['flavor']  != 'object' : continue
+        name='ap1D-{:s}-{:08d}.fits'.format(chan[channel],obj['name'])
+        if os.path.exists(name) and not clobber : 
+            out=CCDData.read(name)
+        else :
+            im=red.reduce(obj['name'],channel=channel,dark=dark)
+            out=trace.extract(im,threads=threads,nout=300)
+            out.write(name,overwrite=True)
+
+    return out
---
+++ python/pyvista/boss.py
@@ -0,0 +1,112 @@
+import numpy as np
+import os
+import pdb
+import matplotlib.pyplot as plt
+from pydl.pydlutils import yanny
+from pyvista import imred, spectra, sdss
+from tools import match,plots
+from ccdproc import CCDData
+import multiprocessing as mp
+
+def visit(planfile,tracefile=None) :
+    """ Reduce BOSS visit
+
+        Driver for parallell processing of b and r channels
+        Makes plots of median counts vs mag
+    """
+    # reduce b1 and r1 in parallel
+    procs=[]
+    for channel in [0,1] :
+        kw={'planfile' : planfile, 'channel' : channel, 'clobber' : False}
+        procs.append(mp.Process(target=do_visit,kwargs=kw))
+    for proc in procs : proc.start()
+    for proc in procs : proc.join()
+
+    plan=yanny.yanny(planfile)
+
+    objs=np.where(plan['SPEXP']['flavor'] == b'science')[0]
+    fig,ax=plots.multi(1,2)
+    allmags=[]
+    allinst=[]
+    for channel in [0,1] :
+      mags=[]
+      inst=[]
+      for obj in objs :
+        name=plan['SPEXP']['name'][obj][channel].astype(str)
+        print(name)
+        out=CCDData.read(name.replace('sdR','sp1D'))
+        mapname=plan['SPEXP']['mapname'][obj].astype(str)
+        if mapname == 'fps' :
+            plug=sdss.config(out.header['CONFID'],specid=1)
+            isky=np.where(plug['category'] == b'sky_boss')[0]
+        else :
+            plug=sdss.config(os.environ['MAPPER_DATA_N']+'/'+mapname.split('-')[1]+'/plPlugMapM-'+mapname+'.par',specid=1,struct='PLUGMAPOBJ')
+            isky=np.where(plug['objType'] == b'SKY')[0]
+        i1,i2=match.match(np.arange(500)+1,plug['fiberId'])
+        if channel == 0 : 
+            mag='g'
+            imag=1
+        else : 
+            mag='i'
+            imag=3
+        skyfiber=plug['fiberId'][isky]
+        sky=np.median(out.data[skyfiber-1,:])
+        print(len(skyfiber),sky)
+        rad=np.sqrt(plug['xFocal'][i2]**2+plug['yFocal'][i2]**2)
+        plots.plotp(ax[channel],plug['mag'][i2,imag],2.5*np.log10(np.median((out.data-sky)/out.header['EXPTIME'],axis=1))[i1],color=None,
+                    zr=[0,300],xr=[10,20],yr=[0,5],size=20,label=name,xt=mag,yt='-2.5*log(cnts/exptime)')
+        mags.append(plug['mag'][i2,imag])
+        inst.append(-2.5*np.log10(np.median((out.data-sky)/out.header['EXPTIME'],axis=1))[i1])
+      ax[channel].grid()
+      ax[channel].legend()
+      allmags.append(mags)
+      allinst.append(inst)
+    fig.suptitle(planfile)
+    fig.tight_layout()
+    fig.savefig(planfile.replace('.par','.png'))
+    return allmags,allinst
+    
+def do_visit(planfile=None,channel=0,clobber=False,nfibers=50) :
+    """ Read raw image (eventually, 2D calibration and extract,
+        using specified flat/trace
+    """
+    plan=yanny.yanny(planfile)
+
+    # are all files already created?
+    objs=np.where(plan['SPEXP']['flavor'] == b'science')[0]
+    for obj in objs :
+        name=plan['SPEXP']['name'][obj][channel].astype(str)
+        if not os.path.exists(name) or clobber :  done = False
+    if done :  return
+
+    # set up Reducer
+    red=imred.Reducer('BOSS',dir=os.environ['BOSS_SPECTRO_DATA_N']+'/'+plan['MJD'])
+   
+    # make Trace/PSF 
+    iflat=np.where(plan['SPEXP']['flavor'] == b'flat')[0]
+    name=plan['SPEXP']['name'][iflat][0][channel].astype(str)
+    if os.path.exists(name.replace('sdR','spTrace')) and not clobber : 
+        trace=spectra.Trace('./'+name.replace('sdR','spTrace')) 
+    else :
+        flat=red.reduce(name,channel=0)
+        trace=spectra.Trace(transpose=red.transpose,rad=3,lags=np.arange(-3,4))
+        ff=np.sum(flat.data[2000:2100],axis=0)
+        if channel==0 : thresh=0.4e6
+        else : thresh=0.2e6
+        peaks,fiber=spectra.findpeak(ff,thresh=thresh)
+        print('found {:d} peaks'.format(len(peaks)))
+        trace.trace(flat,peaks[0:nfibers],index=fiber[0:nfibers])
+        trace.write(name.replace('sdR','spTrace')) 
+
+    # reduce and extract science frames
+    objs=np.where(plan['SPEXP']['flavor'] == b'science')[0]
+    for obj in objs :
+        name=plan['SPEXP']['name'][obj][channel].astype(str)
+        if os.path.exists(name.replace('sdR','sp1D')) and not clobber : 
+            out=CCDData.read(name.replace('sdR','sp1D'))
+        else :
+            im=red.reduce(name,channel=channel)
+            out=trace.extract(im,threads=1,nout=500)
+            out.write(name.replace('sdR','sp1D'),overwrite=True)
+
+    return out
