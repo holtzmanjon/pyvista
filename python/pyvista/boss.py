b64f8713b4341566c62701de5dd09944518bf831
Jon Holtzman <u0914351@apogee.wasatch.peaks>
2022-06-03 16:02:37 -0600

    fix Time

--- python/pyvista/boss.py
+++ python/pyvista/boss.py
@@ -9,6 +9,7 @@ from ccdproc import CCDData
 import multiprocessing as mp
 from astropy.table import Table
 from astropy.io import fits
+from astropy.time import Time
 
 def visit(planfile,tracefile=None,clobber=False,db=None, schema='obs2',maxobj=None) :
     """ Reduce BOSS visit
@@ -116,7 +117,7 @@ def visit(planfile,tracefile=None,clobber=False,db=None, schema='obs2',maxobj=No
                 seeing = 0.
                 if gcam != None :
                     mjd_obs = Time(out.header['DATE-OBS']).mjd
-                    gnearest = np.argmin(np.abs(gcam['mjd']-dateobs))
+                    gnearest = np.argmin(np.abs(gcam['mjd']-mjd_obs))
                     gd = np.where(gcam['nstars'][gnearest]>0)[0]
                     seeing = np.median(gcam['fwhm'][gnearest][gd])
                 elif plates != None :
