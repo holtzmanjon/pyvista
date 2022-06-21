20f8afa9074cb0a4cec67add3a9b47cf54ae20e7
Jon Holtzman <u0914351@apogee.wasatch.peaks>
2022-06-21 12:08:26 -0600

    mods for switching to new Data class BOSS mods

---
+++ python/pyvista/bitmask.py
@@ -0,0 +1,190 @@
+"""
+Routines for handling ASPCAP bitmasks
+"""
+
+import numpy as np
+import pdb
+import sys
+
+class BitMask():
+    '''
+    Base class for bitmasks, define common methods
+    
+    At a minimum, a BitMask will have a set of name, level, descrip
+
+    BitMask provides 3 methods:
+        getname(val,level=level) : returns name(s) of all set bits
+                                  (optionally, of requested level)
+        getval(name) : returns value of bit with input name
+        badval()     : returns value of all bits that are marked bad (level=1)
+        warnval()    : returns value of all bits that are marked warn (level=2)
+    '''
+
+    def getname(self,val,level=0,strip=True):
+        '''
+        Given input value, returns names of all set bits, optionally of a given level
+        '''
+        strflag=''
+        for ibit,name in enumerate(self.name) :
+            if (name != 'RESERVED' ) :
+                try:
+                    if ( val & 2**ibit ) > 0 and ( level == 0 or self.level == level ) :
+                      strflag = strflag + name +','
+                except: 
+                    print('bit problem: ', ibit)
+                    pdb.set_trace()
+        if strip : return strflag.strip(',')
+        else : return strflag
+
+    def getval(self,name) :
+        """
+        Get the numerical bit value of a given character name(s)
+        """
+        if type(name) is str :
+            name = [name]
+        bitval = np.int64(0)
+        for n in name :
+            try:
+                j=self.name.index(n.strip())
+                bitval|=np.int64(2**j)
+            except :
+                print('WARNING: undefined name: ',n)
+        return bitval
+
+
+    def badval(self) :
+        """
+        Return bitmask value of all bits that indicate BAD in input bitmask
+        """
+        val=np.int64(0)
+        for i,level in enumerate(self.level) :
+            if level == 1 :
+                try: val=val | np.int64(2**i)
+                except: pdb.set_trace()
+        return val
+
+    def warnval(self) :
+        """
+        Return bitmask value of all bits that indicate BAD in input bitmask
+        """
+        val=np.int64(0)
+        for i,level in enumerate(self.level) :
+            if level == 2 :
+                val=val | np.int64(2**i)
+        return val
+
+    def print(self,fmt='txt',fp=sys.stdout) :
+        """ Formatted output of bit definitions
+        """
+        if fmt == 'txt' :
+            fp.write('{:25s}{:>6s}  {:s}'.format('Name','Bit','Description'))
+        elif fmt == 'wiki' :
+            fp.write('||{:25s}||{:>6s}||{:s}||'.format('Name','Bit','Description'))
+        elif fmt == 'latex' :
+            fp.write('{:25s}&{:>6s}&{:s}\\\\'.format('Name','Bit','Description'))
+        elif fmt == 'par' :
+            fp.write('masktype {:s} {:d}\n'.format(self.flagname,len(self.name)))
+        elif fmt == 'html' or fmt == 'sdsshtml' :
+            if fmt == 'html' :
+                fp.write("[SDSS_GROUP TITLE='<h2 id={:s}>{:s}']\n".format(self.shorttitle,self.title))
+                fp.write("{:s}\n".format(self.blurb))
+            else :
+                fp.write('<div id="{:s}" class="panel panel-default">\n'.format(self.flagname))
+                fp.write('<div class="panel-heading">\n')
+                fp.write('<h3 class="panel-title"><a class="accordion-toggle" href="#collapse{:s}" data-toggle="collapse" data-parent="#accordion-bitmask">{:s}&nbsp; </a></h3>\n'.format(self.shorttitle,self.title))
+                fp.write('</div>\n')
+                fp.write('<div id="collapse{:s}" class="panel-collapse collapse">\n'.format(self.flagname))
+                fp.write('<div class="panel-body">\n')
+
+            fp.write('<table class="table table-bordered table-condensed"\n')
+            fp.write('<thead>\n')
+            fp.write('<tr><th style="white-space:nowrap;">Bit&nbsp;Name</th><th style="white-space:nowrap;">Binary&nbsp;Digit</th><th>Description</th></tr>\n')
+            fp.write('</thead>\n')
+            fp.write('<tbody>\n')
+        for ibit,name in enumerate(self.name) :
+            if (name != 'RESERVED' and name != '' ) :
+                if fmt == 'txt' :
+                    fp.write('{:25s}{:6d}  {:s}'.format(name,ibit,self.descrip[ibit]))
+                elif fmt == 'wiki' :
+                    fp.write('||{:25s}||{:6d}||{:s}||'.format(name,ibit,self.descrip[ibit]))
+                elif fmt == 'latex' :
+                    fp.write('{:25s}&{:6d}&{:s}\\\\'.format(name,ibit,self.descrip[ibit]))
+                elif fmt == 'par' :
+                    fp.write('maskbits {:s} {:d} {:s} "{:s}"\n'.format(self.flagname,ibit,name,self.descrip[ibit]))
+                elif fmt == 'html' :
+                    fp.write('<tr><td style="white-space:nowrap;">{:s}<td>{:d}<td>{:s}\n'.format(name,ibit,self.descrip[ibit]))
+        if fmt == 'html' or fmt == 'sdsshtml' :
+            fp.write('</tbody>\n')
+            fp.write('</table>\n')
+            if fmt == 'html' :
+                fp.write("[/SDSS_GROUP]\n")
+            else :
+                fp.write("</div>\n")
+                fp.write("</div>\n")
+                fp.write("</div>\n")
+
+class PixelBitMask(BitMask) :
+    '''
+    BitMask class for APOGEE pixel bitmask (APOGEE_PIXMASK)
+    '''
+    flagname='PIXMASK'
+    shorttitle='PixelBitMask'
+    title='PIXMASK : Abitmask for individual pixels'
+    blurb='This bitmask is used to provide information associated with individual pixels in a one-dimensional spectrum. At the visit level, <code>PIXMASK</code> refers to the individual spectrum. At the combined level, PIXMASK attempts to appropriately combine the PIXMASKs of the visit spectrum level. '
+    name=(['BADPIX','CRPIX','SATPIX','UNFIXABLE','BADDARK','BADFLAT','BADERR','NOSKY',
+          'LITTROW_GHOST','PERSIST_HIGH','PERSIST_MED','PERSIST_LOW','SIG_SKYLINE','SIG_TELLURIC','NOT_ENOUGH_PSF','',
+          'INACTIVE_PIXEL','','','','','','','',
+          'BAD_EXTRACTION','','','','','','','RESERVED'])
+
+    level=([1,1,1,1,1,1,1,1,
+            0,0,0,0,0,0,1,0,
+            1,0,0,0,0,0,0,0,
+            1,0,0,0,0,0,0,0])
+
+    maskcontrib=([0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,
+                 0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.0,
+                 0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.0,
+                 0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.0])
+
+    descrip=([
+     'Pixel marked as BAD in bad pixel mask or from strong persistence jump',
+     'Pixel marked as cosmic ray in ap3d',
+     'Pixel marked as saturated in ap3d',
+     'Pixel marked as unfixable in ap3d',
+     'Pixel marked as bad as determined from dark frame',
+     'Pixel marked as bad as determined from flat frame',
+     'Pixel set to have very high error (not used)',
+     'No sky available for this pixel from sky fibers',
+     'Pixel falls in Littrow ghost, may be affected',
+     'Pixel falls in high persistence region, may be affected',
+     'Pixel falls in medium persistence region, may be affected',
+     'Pixel falls in low persistence region, may be affected',
+     'Pixel falls near sky line that has significant flux compared with object',
+     'Pixel falls near telluric line that has significant absorption',
+     'Less than 50 percent PSF in good pixels',
+     '',
+     'Pixel masked by FERRE mask < 0.001',
+     '',
+     '',
+     '',
+     '',
+     '',
+     '',
+     '',
+     '',
+     '',
+     '',
+     '',
+     '',
+     '',
+     '',
+     ''
+    ])
+
+def print_bitmasks(fmt='html',out=None) :
+  
+    if out is not None : fp = open(out,'w') 
+    else : fp  = sys.stdout
+
+    for mask in [ PixelBitMask() ] :
+        mask.print(fmt=fmt,fp=fp)
---
+++ python/pyvista/dataclass.py
@@ -0,0 +1,355 @@
+# Here we define a Data class which is very similar to astropy CCDData and uses
+# much of the astropy code, but defines a class that can also include a wavelength
+# extension. We also define the mask attribute to be a short integer rather than
+# a boolean to allow it to be used as a bitmask. We also remove the requirement
+# of including units. Finally, the name CCDData is not used, since we may 
+# be using digital data that may not come from a CCD!
+
+from astropy.io import registry, fits
+from astropy.nddata import ccddata, CCDData
+import astropy.units as u
+from astropy import log
+from tools import plots
+import numpy as np
+import matplotlib
+import pdb
+
+from astropy.nddata.nduncertainty import (StdDevUncertainty, NDUncertainty, VarianceUncertainty, InverseVariance)
+_known_uncertainties = (StdDevUncertainty, VarianceUncertainty, InverseVariance)
+_unc_name_to_cls = {cls.__name__: cls for cls in _known_uncertainties}
+_unc_cls_to_name = {cls: cls.__name__ for cls in _known_uncertainties}
+ccddata._config_ccd_requires_unit = False
+
+class Data(CCDData) :
+    """ Class to include a wavelength array on top of CCDData
+
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+
+    """
+    def __init__(self, *args, **kwd):
+
+        # Add bitmask attribute
+        if 'bitmask' in kwd :
+            self.bitmask = kwd['bitmask']
+            kwd.pop('bitmask')
+        else :
+            self.bitmask = None
+
+        # Add wavelength attribute
+        if 'wave' in kwd :
+            self.wave = kwd['wave']
+            kwd.pop('wave')
+        else :
+            self.wave = None
+
+        super().__init__(*args, **kwd)
+
+    def to_hdu(self, hdu_bitmask='BITMASK', hdu_uncertainty='UNCERT',
+               hdu_wave='WAVE', wcs_relax=True,
+               key_uncertainty_type='UTYPE', as_image_hdu=True):
+        """Creates an HDUList object from a CCDData object.
+
+        Parameters
+        ----------
+        hdu_bitmask, hdu_uncertainty, hdu_flags : str or None, optional
+            If it is a string append this attribute to the HDUList as
+            `~astropy.io.fits.ImageHDU` with the string as extension name.
+            Flags are not supported at this time. If ``None`` this attribute
+            is not appended.
+            Default is ``'BITMASK'`` for mask, ``'UNCERT'`` for uncertainty
+
+        wcs_relax : bool
+            Value of the ``relax`` parameter to use in converting the WCS to a
+            FITS header using `~astropy.wcs.WCS.to_header`. The common
+            ``CTYPE`` ``RA---TAN-SIP`` and ``DEC--TAN-SIP`` requires
+            ``relax=True`` for the ``-SIP`` part of the ``CTYPE`` to be
+            preserved.
+
+        key_uncertainty_type : str, optional
+            The header key name for the class name of the uncertainty (if any)
+            that is used to store the uncertainty type in the uncertainty hdu.
+            Default is ``UTYPE``.
+
+        as_image_hdu : bool
+            If this option is `True`, the first item of the returned
+            `~astropy.io.fits.HDUList` is a `~astropy.io.fits.ImageHDU`, instead
+            of the default `~astropy.io.fits.PrimaryHDU`.
+
+        Raises
+        ------
+        ValueError
+            - If ``self.mask`` is set but not a `numpy.ndarray`.
+            - If ``self.uncertainty`` is set but not a astropy uncertainty type.
+            - If ``self.uncertainty`` is set but has another unit then
+              ``self.data``.
+
+        NotImplementedError
+            Saving flags is not supported.
+
+        Returns
+        -------
+        hdulist : `~astropy.io.fits.HDUList`
+        """
+        if isinstance(self.header, fits.Header):
+            # Copy here so that we can modify the HDU header by adding WCS
+            # information without changing the header of the CCDData object.
+            header = self.header.copy()
+        else:
+            # Because _insert_in_metadata_fits_safe is written as a method
+            # we need to create a dummy CCDData instance to hold the FITS
+            # header we are constructing. This probably indicates that
+            # _insert_in_metadata_fits_safe should be rewritten in a more
+            # sensible way...
+            dummy_ccd = CCDData([1], meta=fits.Header(), unit="adu")
+            for k, v in self.header.items():
+                dummy_ccd._insert_in_metadata_fits_safe(k, v)
+            header = dummy_ccd.header
+        if self.unit is not u.dimensionless_unscaled:
+            header['bunit'] = self.unit.to_string()
+        if self.wcs:
+            # Simply extending the FITS header with the WCS can lead to
+            # duplicates of the WCS keywords; iterating over the WCS
+            # header should be safer.
+            #
+            # Turns out if I had read the io.fits.Header.extend docs more
+            # carefully, I would have realized that the keywords exist to
+            # avoid duplicates and preserve, as much as possible, the
+            # structure of the commentary cards.
+            #
+            # Note that until astropy/astropy#3967 is closed, the extend
+            # will fail if there are comment cards in the WCS header but
+            # not header.
+            wcs_header = self.wcs.to_header(relax=wcs_relax)
+            header.extend(wcs_header, useblanks=False, update=True)
+
+         
+        if as_image_hdu:
+            hdus = [fits.PrimaryHDU(header=header)]
+            hdus.append(fits.ImageHDU(self.data))
+        else:
+            hdus = [fits.PrimaryHDU(self.data, header)]
+
+        if hdu_uncertainty and self.uncertainty is not None:
+            # We need to save some kind of information which uncertainty was
+            # used so that loading the HDUList can infer the uncertainty type.
+            # No idea how this can be done so only allow StdDevUncertainty.
+            uncertainty_cls = self.uncertainty.__class__
+            if uncertainty_cls not in _known_uncertainties:
+                raise ValueError('only uncertainties of type {} can be saved.'
+                                 .format(_known_uncertainties))
+            uncertainty_name = _unc_cls_to_name[uncertainty_cls]
+
+            hdr_uncertainty = fits.Header()
+            hdr_uncertainty[key_uncertainty_type] = uncertainty_name
+
+            # Assuming uncertainty is an StdDevUncertainty save just the array
+            # this might be problematic if the Uncertainty has a unit differing
+            # from the data so abort for different units. This is important for
+            # astropy > 1.2
+            if (hasattr(self.uncertainty, 'unit') and
+                    self.uncertainty.unit is not None):
+                if not ccddata._uncertainty_unit_equivalent_to_parent(
+                        uncertainty_cls, self.uncertainty.unit, self.unit):
+                    raise ValueError(
+                        'saving uncertainties with a unit that is not '
+                        'equivalent to the unit from the data unit is not '
+                        'supported.')
+
+            hduUncert = fits.ImageHDU(self.uncertainty.array, hdr_uncertainty,
+                                      name=hdu_uncertainty)
+            print('appending uncertainty')
+            hdus.append(hduUncert)
+
+        if hdu_bitmask and self.bitmask is not None:
+            # Always assuming that the mask is a np.ndarray (check that it has
+            # a 'shape').
+            if not hasattr(self.bitmask, 'shape'):
+                raise ValueError('only a numpy.ndarray mask can be saved.')
+
+            # Convert boolean mask to uint since io.fits cannot handle bool.
+            hduMask = fits.ImageHDU(self.bitmask, name=hdu_bitmask)
+            print('appending bitmask')
+            hdus.append(hduMask)
+
+
+        if hdu_wave and self.wave is not None :
+            print('appending wave')
+            hdus.append(fits.ImageHDU(self.wave,name=hdu_wave))
+
+        hdulist = fits.HDUList(hdus)
+
+        return hdulist
+
+    def write(self,file,overwrite=True,png=False) :
+        """  Write SpecData to file
+        """
+        self.to_hdu().writeto(file,overwrite=overwrite)
+
+        if png :
+            #backend=matplotlib.get_backend()
+            #matplotlib.use('Agg')
+            fig,ax=plots.multi(1,1,figsize=(18,6))
+            self.plot(ax)
+            fig.savefig(file+'.png')
+            #matplotlib.use(backend)
+            plt.close()
+
+    def plot(self,ax,rows=None,**kwargs) :
+        if self.data.ndim == 1 :
+            gd = np.where(self.bitmask == False)[0]
+            plots.plotl(ax,self.wave[gd],self.data[gd],**kwargs)
+        else :
+            if rows is None : rows=range(self.wave.shape[0])
+            for row in rows :
+                gd = np.where(self.bitmask[row,:] == False)[0]
+                plots.plotl(ax,self.wave[row,gd],self.data[row,gd],**kwargs)
+        #gd = np.where(self.bitmask == False)[0]
+        #med=np.nanmedian(self.data[gd])
+        #ax.set_ylim(0,2*med)
+
+
+def fits_data_reader(filename, hdu=0, unit=None, hdu_uncertainty='UNCERT',
+                        hdu_bitmask='BITMASK', hdu_wave='WAVE',
+                        key_uncertainty_type='UTYPE', **kwd):
+    """
+    Generate a Data object from a FITS file. Modified from astropy fits_ccddata_reader
+
+    Parameters
+    ----------
+    filename : str
+        Name of fits file.
+
+    hdu : int, str, tuple of (str, int), optional
+        Index or other identifier of the Header Data Unit of the FITS
+        file from which CCDData should be initialized. If zero and
+        no data in the primary HDU, it will search for the first
+        extension HDU with data. The header will be added to the primary HDU.
+        Default is ``0``.
+
+    unit : `~astropy.units.Unit`, optional
+        Units of the image data. If this argument is provided and there is a
+        unit for the image in the FITS header (the keyword ``BUNIT`` is used
+        as the unit, if present), this argument is used for the unit.
+        Default is ``None``.
+
+    hdu_uncertainty : str or None, optional
+        FITS extension from which the uncertainty should be initialized. If the
+        extension does not exist the uncertainty of the CCDData is ``None``.
+        Default is ``'UNCERT'``.
+
+    hdu_bitmask : str or None, optional
+        FITS extension from which the bitmask should be initialized. If the
+        extension does not exist the bitmask of the CCDData is ``None``.
+        Default is ``'BITMASK'``.
+
+    hdu_flags : str or None, optional
+        Currently not implemented.
+        Default is ``None``.
+
+    key_uncertainty_type : str, optional
+        The header key name where the class name of the uncertainty  is stored
+        in the hdu of the uncertainty (if any).
+        Default is ``UTYPE``.
+
+    kwd :
+        Any additional keyword parameters are passed through to the FITS reader
+        in :mod:`astropy.io.fits`; see Notes for additional discussion.
+
+    Notes
+    -----
+    FITS files that contained scaled data (e.g. unsigned integer images) will
+    be scaled and the keywords used to manage scaled data in
+    :mod:`astropy.io.fits` are disabled.
+    """
+    unsupport_open_keywords = {
+        'do_not_scale_image_data': 'Image data must be scaled.',
+        'scale_back': 'Scale information is not preserved.'
+    }
+    for key, msg in unsupport_open_keywords.items():
+        if key in kwd:
+            prefix = f'unsupported keyword: {key}.'
+            raise TypeError(' '.join([prefix, msg]))
+    with fits.open(filename, **kwd) as hdus:
+        hdr = hdus[hdu].header
+
+        if hdu_uncertainty is not None and hdu_uncertainty in hdus:
+            unc_hdu = hdus[hdu_uncertainty]
+            stored_unc_name = unc_hdu.header.get(key_uncertainty_type, 'None')
+            # For compatibility reasons the default is standard deviation
+            # uncertainty because files could have been created before the
+            # uncertainty type was stored in the header.
+            unc_type = ccddata._unc_name_to_cls.get(stored_unc_name, StdDevUncertainty)
+            uncertainty = unc_type(unc_hdu.data)
+        else:
+            uncertainty = None
+
+        if hdu_bitmask is not None and hdu_bitmask in hdus:
+            # Mask is saved as uint 
+            bitmask = hdus[hdu_bitmask].data.astype(np.uintc)
+        else:
+            bitmask = None
+
+        if hdu_wave is not None and hdu_wave in hdus:
+            # Wavelength is saved as float
+            wave = hdus[hdu_wave].data.astype(np.float32)
+        else:
+            wave = None
+
+        # search for the first instance with data if
+        # the primary header is empty.
+        if hdu == 0 and hdus[hdu].data is None:
+            for i in range(len(hdus)):
+                if (hdus.info(hdu)[i][3] == 'ImageHDU' and
+                        hdus.fileinfo(i)['datSpan'] > 0):
+                    hdu = i
+                    comb_hdr = hdus[hdu].header.copy()
+                    # Add header values from the primary header that aren't
+                    # present in the extension header.
+                    comb_hdr.extend(hdr, unique=True)
+                    hdr = comb_hdr
+                    log.info(f"first HDU with data is extension {hdu}.")
+                    break
+
+        if 'bunit' in hdr:
+            fits_unit_string = hdr['bunit']
+            # patch to handle FITS files using ADU for the unit instead of the
+            # standard version of 'adu'
+            if fits_unit_string.strip().lower() == 'adu':
+                fits_unit_string = fits_unit_string.lower()
+        else:
+            fits_unit_string = None
+
+        if fits_unit_string:
+            if unit is None:
+                # Convert the BUNIT header keyword to a unit and if that's not
+                # possible raise a meaningful error message.
+                try:
+                    kifus = CCDData.known_invalid_fits_unit_strings
+                    if fits_unit_string in kifus:
+                        fits_unit_string = kifus[fits_unit_string]
+                    fits_unit_string = u.Unit(fits_unit_string)
+                except ValueError:
+                    raise ValueError(
+                        'The Header value for the key BUNIT ({}) cannot be '
+                        'interpreted as valid unit. To successfully read the '
+                        'file as CCDData you can pass in a valid `unit` '
+                        'argument explicitly or change the header of the FITS '
+                        'file before reading it.'
+                        .format(fits_unit_string))
+            else:
+                log.info("using the unit {} passed to the FITS reader instead "
+                         "of the unit {} in the FITS file."
+                         .format(unit, fits_unit_string))
+
+        use_unit = unit or fits_unit_string
+        hdr, wcs = ccddata._generate_wcs_and_update_header(hdr)
+        data = Data(hdus[hdu].data, meta=hdr, unit=use_unit,
+                    bitmask=bitmask, uncertainty=uncertainty, wave=wave, wcs=wcs)
+
+    return data
+
+registry.register_reader('fits', Data, fits_data_reader,force=False)
