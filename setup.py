d4fc2d02ba127eb2df4b0e40b1933adc17e2fdbe
Jon Holtzman <holtz@milkyway.nmsu.edu>
2023-03-07 04:22:13 -0700

    update version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='astro-pyvista',
-    version='0.2.0',    
+    version='0.2.1',    
     description='Astronomical image processing',
     url='https://github.com/holtzmanjon/pyvista',
     author='Jon Holtzman',
